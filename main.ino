7b2603668cc349622ba98d210dbd7cc68851219d
mitchellciupak <ciupak@purdue.edu>
2020-07-03 12:06:17 -0500

    Backups

--- main.ino
+++ main.ino
@@ -1,23 +1,29 @@
-// GxEPD_MinimumExample by Jean-Marc Zingg
+// Mitchell Ciupak
+//Project Main
 
+//Includes
 #include <GxEPD.h>
-
-// select the display class to use, only one, copy from GxEPD_Example
 #include <GxGDEH0154D67/GxGDEH0154D67.h>  // 1.54" b/w
-
 #include <GxIO/GxIO_SPI/GxIO_SPI.h>
 #include <GxIO/GxIO.h>
 
-// constructor for AVR Arduino, copy from GxEPD_Example else
+//Macros
+int rainsense= A0; // analog sensor input pin 0
+int countval= 0; // counter value starting from 0 and goes up by 1 every second
+
+//Constructor for AVR Arduino, copy from GxEPD_Example else
 GxIO_Class io(SPI, /*CS=*/ SS, /*DC=*/ 9, /*RST=*/ 8); // arbitrary selection of 8, 9 selected for default of GxEPD_Class
 GxEPD_Class display(io, /*RST=*/ 8, /*BUSY=*/ 7); // default selection of (9), 7
 
-void setup()
-{
-  display.init();
-  display.eraseDisplay();
-  // comment out next line to have no or minimal Adafruit_GFX code
-  display.drawPaged(drawHelloWorld); // version for AVR using paged drawing, works also on other processors
+//Init
+void setup(){
+   Serial.begin(9600); //TODO Ddebug
+   //Prep Rain Sensor
+   pinMode(rainsense, INPUT);
+
+   //Prep Disp
+   display.init();
+   display.eraseDisplay();
 }
 
 void drawHelloWorld()
@@ -26,4 +32,17 @@ void drawHelloWorld()
   display.print("Hello World!");
 }
 
-void loop() {};
+void loop(){
+   int rainSenseReading = analogRead(rainsense);
+   Serial.println(rainSenseReading); // serial monitoring message 
+   delay(250);// rain sensing value from 0 to 1023.
+
+   // from heavy rain - no rain.
+   if (countval >= 35){ 
+      Serial.print("Heavy rain");
+      digitalWrite(buzzerout, HIGH);  //raise an alert after x time
+      digitalWrite(ledout, HIGH);  // led glow
+   }
+};
+
+//https://www.youtube.com/watch?v=UBEhod2wxvE
--- test_raindetection2.ino
+++ test_raindetection2.ino
@@ -1,16 +1,12 @@
 // Mitchell Ciupak
 // Rain Detection Module
 
-//Macros
-int rainsense = A0;
-int buzzerout = 12;
-int countval = 0;
-
-
+int rainsense= A0; // analog sensor input pin 0
+int countval= 0; // counter value starting from 0 and goes up by 1 every second
+ 
 void setup(){
    Serial.begin(9600);
    pinMode(rainsense, INPUT);
-   pinMode(buzzerout, OUTPUT);
 }
 
 void loop(){
@@ -19,17 +15,18 @@ void loop(){
    delay(250);// rain sensing value from 0 to 1023.
 
    // from heavy rain - no rain.
-   if (countval >= 35){ 
-      Serial.print("Heavy rain");
+   if (rainSenseReading < 500){ 
+    
+      Serial.print("IT IS RAINING");
       digitalWrite(buzzerout, HIGH);  //raise an alert after x time
-   }
 
-   if(rainSenseReading < 500) {
-      countval++; 
-   }
-   else {
-      digitalWrite(buzzerout, LOW)
       countval = 0;
    }
-   delay(1000);
+   else{
+    
+      Serial.print("NO RAIN");
+
+      countval = 0;
+   }
+
 }
